d706fd663eb8a1aadf7ce73254fc211f67097a06
Somdev Choudhary <somdevchoudhary@gmail.com>
2025-11-18 13:53:37 +0400

    Initial Commit

---
+++ RealtimePriceTracker/RealtimePriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimePriceTrackerApp.swift
+//  RealtimePriceTracker
+//
+//  Created by Somdev Choudhary on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RealtimePriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
